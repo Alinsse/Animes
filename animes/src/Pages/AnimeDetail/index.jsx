6d9997593a69ac8bff2a6a2f9f0b5fff3dece831
Alinsse <122397276+Alinsse@users.noreply.github.com>
2025-09-18 14:42:22 +0000

    ajustando rotas

--- animes/src/Pages/AnimeDetail/index.jsx
+++ animes/src/Pages/AnimeDetail/index.jsx
@@ -1,32 +1,29 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import { getAnimeDetails } from '../../Services/HomeApis/HomeId/useId.ts';
 
-const AnimeDetails = () => {
-  const { id } = useParams();
+const AnimeDetail = ({ animeId }) => {
   const [anime, setAnime] = useState(null);
 
   useEffect(() => {
     const fetchAnime = async () => {
-      const data = await getAnimeDetails(id);
+      const data = await getAnimeDetails(animeId);
       setAnime(data);
     };
-    fetchAnime();
-  }, [id]);
 
-  if (!anime) {
-    return <p>Carregando...</p>;
-  }
+    fetchAnime();
+  }, [animeId]);
+
+  if (!anime) return <p>Carregando...</p>;
 
   const { titles, synopsis, posterImage } = anime.attributes;
 
   return (
     <div style={{ padding: '20px' }}>
       <h1>{titles.en || titles.en_jp}</h1>
-      <img src={posterImage.medium} alt={titles.en || titles.en_jp} />
+      <img src={posterImage?.medium} alt={titles.en || titles.en_jp} />
       <p>{synopsis}</p>
     </div>
   );
 };
 
-export default AnimeDetails;
+export default AnimeDetail;
--- animes/src/Routes/Routes.js
+++ animes/src/Routes/Routes.js
@@ -4,20 +4,18 @@ import { Routes, Route, useParams } from 'react-router-dom';
 import Home from '../Pages/Home/index';
 import AnimeDetail from '../Pages/AnimeDetail';
 
-function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/anime/:id" element={<AnimeDetail />} />
-    </Routes>
-  );
-}
-
-
 const AnimeDetailWrapper = () => {
   const { id } = useParams();
   return <AnimeDetail animeId={id} />;
 };
 
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/anime/:id" element={<AnimeDetailWrapper />} />
+    </Routes>
+  );
+}
 
 export default AppRoutes;
